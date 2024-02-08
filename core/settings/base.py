3ae12b78ba8b21311a2d14614b5657947a86f4e1
Nate-Soul <nanithsukpong@gmail.com>
2024-02-08 18:41:36 +0100

    added to allowedhosts

--- core/settings/base.py
+++ core/settings/base.py
@@ -21,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-ALLOWED_HOSTS = [ "nate-soul-api.vercel.app" ]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
+
+ALLOWED_HOSTS = [
+    "nate-soul-api.vercel.app", 
+    "127.0.0.1", 
+    "localhost"
+]
 
 # Application definition
 
