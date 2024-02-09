a391c5f26e3794f98acca1ee60da1046ff4c1b67
Nate-Soul <nanithsukpong@gmail.com>
2024-02-09 06:53:25 +0100

    production fixes

--- core/settings/base.py
+++ core/settings/base.py
@@ -31,7 +31,8 @@ DEBUG = os.environ.get("DEBUG") == "True"
 ALLOWED_HOSTS = [
     "nate-soul-api.vercel.app", 
     "127.0.0.1", 
-    "localhost"
+    "localhost",
+    "postgresql-natesoul.alwaysdata.net"
 ]
 
 # Application definition
@@ -121,6 +122,11 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
 
 #media
 MEDIA_URL = '/media/'
--- core/settings/development.py
+++ core/settings/development.py
@@ -10,10 +10,17 @@ DATABASES = {
     }
 }
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("POSTGRES_DB_NAME"),
+#         'HOST': os.environ.get("POSTGRES_DB_HOST"),
+#         'PORT': os.environ.get("POSTGRES_DB_PORT"),
+#         'USER': os.environ.get("POSTGRES_DB_USER"),
+#         'PASSWORD': os.environ.get("POSTGRES_DB_PASS"),
+#     }
+# }
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
