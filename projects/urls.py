2155da4519575a1a1a930d118d889d64cefa1671
Nate-Soul <nanithsukpong@gmail.com>
2024-02-24 08:43:05 +0100

    Added related projects view

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     ProjectListAPIView,
-    ProjectDetailAPIView
+    ProjectDetailAPIView,
+    RelatedProjectsView
 )
 
 app_name = "projects"
 urlpatterns = [
     path('', ProjectListAPIView.as_view(), name="project-list"),
     path('<slug:slug>/', ProjectDetailAPIView.as_view(), name="project-detail"),
+    path('<slug:slug>/related/', RelatedProjectsView.as_view(), name="related-projects"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -24,11 +24,11 @@ class ProjectListAPIView(APIView):
     def get(self, request, *args, **kwargs):
         projects = Project.objects.filter(is_active=True)
         serializer = self.serializer_class(projects, many=True, context={"request": request})
-        context = {
+        response_context = {
             "data": serializer.data,
             "message": "Project list fetched successfully"
         }
-        return Response(context, status=status.HTTP_200_OK)
+        return Response(response_context, status=status.HTTP_200_OK)
     
 class ProjectDetailAPIView(APIView):
     
@@ -44,6 +44,26 @@ class ProjectDetailAPIView(APIView):
         serializer = self.serializer_class(project, context={"request": request})
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class RelatedProjectsView(APIView):
+    
+    serializer_class    = ProjectSerializer
+    permission_classes  = (permissions.IsAuthenticatedOrReadOnly,)
+    model_class         = Project
+    
+    def get_object(self, slug):
+        return get_object_or_404(self.model_class, slug=slug)
+    
+    def get(self, request, slug, *args, **kwargs):
+        project = self.get_object(slug)
+        related_projects = self.model_class.objects.filter(categories__in=project.categories.all()).exclude(id=project.id).distinct()
+        serializer = self.serializer_class(related_projects, many=True, context = {"request": request})
+        response_context = {
+            "data": serializer.data, 
+            "message": "Related projects fetched successfully"
+        }
+        return Response(response_context, status=status.HTTP_200_OK)
+        
 class TagListAPIView(APIView):
     
     model_class        = Tag 
