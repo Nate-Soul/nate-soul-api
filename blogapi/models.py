3806d6e077e4e5cf7c2cd824b21f4ae4ff1ccb13
Nate-Soul <nanithsukpong@gmail.com>
2024-11-17 05:52:13 +0100

    new: initialize blog API

---
+++ blogapi/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class ArticleTag(models.Model):
+    name        = models.CharField(max_length=100, verbose_name=_("Tag name"), unique=True)
+    slug        = models.SlugField(max_length=100, verbose_name=_("Tag safe URL"), unique=True)
+    created     = models.DateTimeField(auto_now_add=True)
+    modified    = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Article Tag")
+        verbose_name_plural = _("Article Tags")
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Article(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        ('archived', 'Archived'),
+    ]
+
+    title = models.CharField(max_length=255, unique=True, verbose_name=_("Article title"))
+    slug  = models.SlugField(max_length=255, unique=True, verbose_name=_("Article Safe URL"))
+    excerpt = models.TextField(verbose_name=_("Article Overview"), null=True, blank=True)
+    excerpt = models.TextField(blank=True, null=True)
+    featured = models.BooleanField(default=False)
+    featured_img_url = models.ImageField(upload_to="images/blog/thumbnails", default="images/blog/default.png", blank=True, null=True)
+    tags = models.ManyToManyField(ArticleTag, related_name="tags")
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    content = models.TextField()
+    parent_post = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True
+    )
+    published_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-published_date']
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+    def __str__(self):
+        return self.title
--- core/settings/base.py
+++ core/settings/base.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'testimonials',
     'processes',
     'accounts',
+    'blogapi',
 ]
 
 MIDDLEWARE = [
